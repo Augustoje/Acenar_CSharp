bc7373b4f2ec3632392b0bedc0a262a75e7ee37a
augustoje <augusto.jesus@outlook.com.br>
2021-10-16 03:49:18 -0300

    Primeiro Commit

--- Acenar_CSharp/Program.cs
+++ Acenar_CSharp/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace Acenar_CSharp
+namespace ConsoleClassePessoa
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("Digite o nome: ");
+            pessoa.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            pessoa.idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite seu RG: ");
+            pessoa.rg = Console.ReadLine();
+
+            Console.WriteLine("Digite seu CPF: ");
+            pessoa.cpf = Console.ReadLine();
+
+            Console.WriteLine("Nome: {0}", pessoa.nome);
+            Console.WriteLine("Idade: {0}", pessoa.idade);
+            Console.WriteLine("Rg: {0}", pessoa.rg);
+            Console.WriteLine("CPF: {0}", pessoa.cpf);
+
+            Pessoa pessoaSimone = new Pessoa();
+            pessoaSimone.nome = "Simone Jesus";
+            pessoaSimone.idade = 35;
+            pessoaSimone.rg = "12.000.000-0";
+            pessoaSimone.cpf = "123.000.000-00";
+
+            Console.WriteLine("Nome: {0}", pessoaSimone.nome);
+            Console.WriteLine("Idade: {0}", pessoaSimone.idade);
+            Console.WriteLine("Nome: {0}", pessoaSimone.rg);
+            Console.WriteLine("Idade: {0}", pessoaSimone.idade);
+
+            pessoa.Acenar(pessoaSimone);
+            pessoa.PerguntaIdade(pessoaSimone);
+
+
+
         }
     }
 }
