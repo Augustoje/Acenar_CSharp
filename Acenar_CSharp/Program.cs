1345c361d06a50cf611eec3a52dbe79aedb62f50
augustoje <augusto.jesus@outlook.com.br>
2021-10-16 03:47:28 -0300

    Adicionar arquivos de projeto.

---
+++ Acenar_CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Acenar_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
